df743b7f947f69bf2ccf88e73e5db0b462f1a400
Kayleigh Zank <kzank@emory.edu>
2025-08-18 15:23:49 -0400

    Table 1 Exercises with gtsummary

---
+++ Table 1 Exercises.R
@@ -0,0 +1,125 @@
+# Load and clean data
+nlsy_cols <- c(
+	"glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+	"id", "nsibs", "samp", "race_eth", "sex", "region",
+	"income", "res_1980", "res_2002", "age_bir"
+)
+nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
+								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+								 skip = 1, col_names = nlsy_cols
+) |>
+	mutate(
+		region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+		sex_cat = factor(sex, labels = c("Male", "Female")),
+		race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+		eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+		glasses_cat = factor(glasses, labels = c("No", "Yes"))
+	)
+
+# Make a tbl_summary(). Include categorical region, race/ethnicity, income, and the sleep variables (use a helper function to select those) and make sure they are nicely labeled.
+tbl_summary(
+	nlsy,
+	include = c(
+		region_cat, race_eth_cat, income,
+		sleep_wkdy, sleep_wknd
+	),
+	#labels
+	label = list(
+		race_eth_cat ~ "Race/Ethnicity",
+		region_cat ~ "Region",
+		income ~ "Income",
+		sleep_wkdy ~ "Weekday Hours of Sleep",
+		sleep_wknd ~ "Weekend Hours of Sleep"
+	),
+	missing_text = "Missing",
+) %>%
+	bold_labels()
+
+# Stratify the table by sex. Add a p-value comparing the sexes and an overall column combining both sexes.
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(
+		starts_with("sleep"),
+		region_cat, race_eth_cat, income
+	),
+	#labels
+	label = list(
+		race_eth_cat ~ "Race/Ethnicity",
+		region_cat ~ "Region",
+		income ~ "Income",
+		sleep_wkdy ~ "Weekday Hours of Sleep",
+		sleep_wknd ~ "Weekend Hours of Sleep"
+	),
+	missing_text = "Missing",
+) %>%
+	bold_labels() %>%
+	add_p(test = list(
+		all_continuous() ~ "t.test",
+		all_categorical() ~ "chisq.test"
+	)) %>%
+	add_overall(col_label = "**Total** N = {N}")
+
+
+# For the income variable, show the 10th and 90th percentiles of income with 3 digits, and for the sleep variables, show the min and the max with 1 digit.
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(
+		starts_with("sleep"),
+		region_cat, race_eth_cat, income
+	),
+	#labels
+	label = list(
+		race_eth_cat ~ "Race/Ethnicity",
+		region_cat ~ "Region",
+		income ~ "Income",
+		sleep_wkdy ~ "Weekday Hours of Sleep",
+		sleep_wknd ~ "Weekend Hours of Sleep"
+	),
+	missing_text = "Missing",
+	statistic = list(c(sleep_wkdy, sleep_wknd) ~ "{mean} ({min}, {max})",
+									 income = "{p10}, {p90}"),
+	digits = c(starts_with("sleep") ~ 1, income ~ 3)
+) %>%
+	bold_labels() %>%
+	add_p(test = list(
+		all_continuous() ~ "t.test",
+		all_categorical() ~ "chisq.test"
+	)) %>%
+	add_overall(col_label = "**Total** N = {N}")
+
+# Add a footnote to the race/ethnicity variable with a link to the page describing how NLSY classified participants, bolding p-value label
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(
+		starts_with("sleep"),
+		region_cat, race_eth_cat, income
+	),
+	#labels
+	label = list(
+		race_eth_cat ~ "Race/Ethnicity",
+		region_cat ~ "Region",
+		income ~ "Income",
+		sleep_wkdy ~ "Weekday Hours of Sleep",
+		sleep_wknd ~ "Weekend Hours of Sleep"
+	),
+	missing_text = "Missing",
+	statistic = list(c(sleep_wkdy, sleep_wknd) ~ "{mean} ({min}, {max})",
+									 income = "{p10}, {p90}"),
+	digits = c(starts_with("sleep") ~ 1, income ~ 3)
+) %>%
+	bold_labels() %>%
+	add_p(test = list(
+		all_continuous() ~ "t.test",
+		all_categorical() ~ "chisq.test"
+	)) %>%
+	add_overall(col_label = "**Total** N = {N}") %>%
+	modify_table_styling(
+		footnote = "See https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data",
+		columns = "label",
+		rows = label == "Race/Ethnicity"
+		) %>%
+	modify_header(p.value = "**P-Value**")
